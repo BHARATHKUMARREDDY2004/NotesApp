45390dcd4b14b438d248a6a111f31e9b5e3362e1
Bharath Kumar Reddy Vemireddy <vemireddybharath@lpu.in>
2025-01-09 10:34:23 +0530

    add to cart feature added in item-info.

--- app/(root)/(other)/item-info.tsx
+++ app/(root)/(other)/item-info.tsx
@@ -1,3 +1,142 @@
+// import React, { useState } from "react";
+// import {
+//   View,
+//   SafeAreaView,
+//   Text,
+//   Image,
+//   ScrollView,
+//   TouchableOpacity,
+// } from "react-native";
+// import { useLocalSearchParams } from "expo-router";
+// import { AntDesign } from "@expo/vector-icons";
+// import Swiper from 'react-native-swiper';
+// import { useStore } from "@/store"; // Import your Zustand store
+
+// const ItemInfo = () => {
+//   const { item } = useLocalSearchParams();
+//   const itemData = JSON.parse(item as string);
+//   const [selectedUnit, setSelectedUnit] = useState(itemData.units[0]);
+//   const [selectedUnitPrice, setSelectedUnitPrice] = useState(itemData.unitPrices[0]);
+
+//   const { addToCart } = useStore(); // Accessing addToCart from the store
+
+//   const handleAdd = (unit: string, price: number) => {
+//     addToCart({
+//       id: itemData.id,
+//       name: itemData.name,
+//       unit,
+//       quantity: 1,
+//       price,
+//       image: itemData.images[0],
+//     });
+//   };
+
+//   console.log(selectedUnit, selectedUnitPrice);
+
+//   return (
+//     <SafeAreaView className="flex-1 bg-white">
+//       <ScrollView className="flex-1">
+//         {/* Back Button */}
+//         {/* <TouchableOpacity
+//           className="absolute left-4 top-4 z-10 bg-white rounded-full p-2"
+//           onPress={() => router.back()}
+//         >
+//           <AntDesign name="left" size={24} color="black" />
+//         </TouchableOpacity> */}
+
+//         {/* Product Images */}
+//         <View className="h-96 bg-white relative">
+//           <Swiper
+//             loop={false}
+//             dot={<View className="w-1 h-1 rounded-full bg-gray-300 mx-[.8px]" />}
+//             activeDot={<View className="w-1.5 h-1.5 rounded-full bg-orange-500 mx-[1px]" />}
+//             paginationStyle={{
+//               bottom: 10,
+//               right: 10,
+//               left: undefined,
+//               flexDirection: 'row',
+//             }}
+//           >
+//             {itemData.images.map((image: string, index: number) => (
+//               <View key={index} className="flex-1 justify-center items-center">
+//                 <Image
+//                   source={{ uri: image }}
+//                   className="w-full h-full"
+//                   resizeMode="contain"
+//                 />
+//               </View>
+//             ))}
+//           </Swiper>
+//         </View>
+
+//         {/* Product Info */}
+//         <View className="p-4">
+//           <Text className="text-xl font-semibold text-gray-800 mb-2">
+//             {itemData.name}
+//           </Text>
+
+//           {/* Unit Selection */}
+//           <Text className="text-md font-medium mb-4 text-gray-800">
+//             Select Unit
+//           </Text>
+//           <View className="flex-row flex-wrap gap-4">
+//             {itemData.units.map((unit: string, index: number) => {
+//               const isSelected = selectedUnit === unit;
+//               return (
+//                 <TouchableOpacity
+//                   key={unit}
+//                   onPress={() => {setSelectedUnit(unit); setSelectedUnitPrice(itemData.unitPrices[index])}}
+//                   className={`border-2 rounded-lg p-1 w-[90px] border-[0.8px] ${
+//                     isSelected
+//                       ? "border-orange-500 bg-orange-50"
+//                       : "border-gray-100"
+//                   }`}
+//                 >
+//                   <Text className="text-center text-md text-gray-800">
+//                     {unit}
+//                   </Text>
+//                   <Text className="text-center text-md font-semibold text-gray-800">
+//                     ₹{itemData.unitPrices[index]}
+//                   </Text>
+//                 </TouchableOpacity>
+//               );
+//             })}
+//           </View>
+
+//           {/* Minimum Quantity */}
+//           <View className="mt-6 p-4 bg-gray-50 rounded-lg">
+//             <Text className="text-gray-600">
+//               Minimum Order Quantity: {itemData.minQty} units
+//             </Text>
+//           </View>
+
+//           {/* Product Information */}
+//           <View className="mt-6">
+//             <Text className="text-xl font-medium mb-4 text-gray-800">
+//               Product Information
+//             </Text>
+//             {itemData.information.map((info: string, index: number) => (
+//               <View
+//                 key={info}
+//                 className="flex-row justify-between py-3 border-b border-gray-200"
+//               >
+//                 <Text className="text-gray-600">{info}</Text>
+//                 <Text className="text-gray-800">
+//                   {itemData.infoDescriptions[index]}
+//                 </Text>
+//               </View>
+//             ))}
+//           </View>
+//         </View>
+//       </ScrollView>
+
+//       <View className="pb-[100px]" />
+//     </SafeAreaView>
+//   );
+// };
+
+// export default ItemInfo;
+
 import React, { useState } from "react";
 import {
   View,
@@ -8,36 +147,49 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import { AntDesign } from "@expo/vector-icons";
-import Swiper from 'react-native-swiper';
+import Swiper from "react-native-swiper";
+import { useStore } from "@/store"; // Import Zustand store
+import { LinearGradient } from "expo-linear-gradient";
 
 const ItemInfo = () => {
   const { item } = useLocalSearchParams();
   const itemData = JSON.parse(item as string);
   const [selectedUnit, setSelectedUnit] = useState(itemData.units[0]);
+  const [selectedUnitPrice, setSelectedUnitPrice] = useState(
+    itemData.unitPrices[0]
+  );
+
+  const { addToCart } = useStore(); // Accessing addToCart from the store
+
+  const handleAdd = () => {
+    addToCart({
+      id: itemData.id,
+      name: itemData.name,
+      unit: selectedUnit,
+      quantity: 1,
+      price: selectedUnitPrice,
+      image: itemData.images[0],
+    });
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <ScrollView className="flex-1">
-        {/* Back Button */}
-        {/* <TouchableOpacity 
-          className="absolute left-4 top-4 z-10 bg-white rounded-full p-2"
-          onPress={() => router.back()}
-        >
-          <AntDesign name="left" size={24} color="black" />
-        </TouchableOpacity> */}
-
         {/* Product Images */}
         <View className="h-96 bg-white relative">
           <Swiper
             loop={false}
-            dot={<View className="w-1 h-1 rounded-full bg-gray-300 mx-[.8px]" />}
-            activeDot={<View className="w-1.5 h-1.5 rounded-full bg-orange-500 mx-[1px]" />}
+            dot={
+              <View className="w-1 h-1 rounded-full bg-gray-300 mx-[.8px]" />
+            }
+            activeDot={
+              <View className="w-1.5 h-1.5 rounded-full bg-orange-500 mx-[1px]" />
+            }
             paginationStyle={{
               bottom: 10,
               right: 10,
               left: undefined,
-              flexDirection: 'row',
+              flexDirection: "row",
             }}
           >
             {itemData.images.map((image: string, index: number) => (
@@ -58,6 +210,12 @@ const ItemInfo = () => {
             {itemData.name}
           </Text>
 
+          
+          {/* Minimum Quantity */}
+            <Text className="text-sm text-gray-600 mb-2 rounded-lg">
+              Minimum Order Quantity: {itemData.minQty} units
+            </Text>
+
           {/* Unit Selection */}
           <Text className="text-md font-medium mb-4 text-gray-800">
             Select Unit
@@ -68,55 +226,60 @@ const ItemInfo = () => {
               return (
                 <TouchableOpacity
                   key={unit}
-                  onPress={() => setSelectedUnit(unit)}
-                  className={`border-2 rounded-lg p-1 w-[90px] ${
+                  onPress={() => {
+                    setSelectedUnit(unit);
+                    setSelectedUnitPrice(itemData.unitPrices[index]);
+                  }}
+                  className={`border-2 rounded-lg p-1 w-[90px] border-[0.8px] ${
                     isSelected
-                      ? "border-green-500 bg-green-50"
-                      : "border-gray-200"
+                      ? "border-orange-500 bg-orange-50"
+                      : "border-gray-100"
                   }`}
                 >
-                  <Text className="text-center text-md">
+                  <Text className="text-center text-md text-gray-800">
                     {unit}
                   </Text>
-                  <Text className="text-center text-md font-semibold">
+                  <Text className="text-center text-md font-semibold text-gray-800">
                     ₹{itemData.unitPrices[index]}
                   </Text>
                 </TouchableOpacity>
               );
             })}
           </View>
-
-          {/* Minimum Quantity */}
-          <View className="mt-6 p-4 bg-gray-50 rounded-lg">
-            <Text className="text-gray-600">
-              Minimum Order Quantity: {itemData.minQty} units
-            </Text>
-          </View>
-
-          {/* Product Information */}
-          <View className="mt-6">
-            <Text className="text-xl font-medium mb-4 text-gray-800">
-              Product Information
-            </Text>
-            {itemData.information.map((info: string, index: number) => (
-              <View
-                key={info}
-                className="flex-row justify-between py-3 border-b border-gray-200"
-              >
-                <Text className="text-gray-600">{info}</Text>
-                <Text className="text-gray-800">
-                  {itemData.infoDescriptions[index]}
-                </Text>
-              </View>
-            ))}
-          </View>
         </View>
       </ScrollView>
 
-      <View className="pb-[100px]" />
+      {/* Bottom Section */}
+      <View className="absolute bottom-0 left-0 right-0 bg-white shadow-md px-4 py-2 flex-row items-center justify-between">
+        <LinearGradient
+          colors={["#FFEBD7", "transparent"]} // Dark at the base, fades outward
+          start={{ x: 0, y: 1 }} // Starts at the bottom (near the section)
+          end={{ x: 0, y: 0.3 }} // Ends at the top (fading away)
+          style={{
+            position: "absolute",
+            top: -10, // Move above the section
+            left: 0,
+            right: 0,
+            height: 10, // Adjust the height to control the shadow distance
+          }}
+        />
+
+        <View>
+          <Text className="text-psemibold text-gray-800">{selectedUnit}</Text>
+          <Text className="text-sm text-gray-800 font-semibold">
+            MRP ₹{selectedUnitPrice}
+          </Text>
+          <Text className="text-xs text-gray-500">Inclusive of all taxes</Text>
+        </View>
+        <TouchableOpacity
+          onPress={handleAdd}
+          className="bg-green-500 py-3 px-6 rounded-lg"
+        >
+          <Text className="text-white font-psemibold">Add to cart</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default ItemInfo;
-
